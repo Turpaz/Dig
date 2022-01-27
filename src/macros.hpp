07bbe38f57530d407ee60d7697f5e4896bdafe39
Turpaz <eitan.turpaz@gmail.com>
2022-01-28 01:50:21 +0200

    command line arguments (flags and path) are now working

--- src/macros.hpp
+++ src/macros.hpp
@@ -1,21 +1,31 @@
 #ifndef MACROS_HPP
 #define MACROS_HPP
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define DIG_VERSION "1.0.0"
 
-#define USAGE_HELPER "Usage: \"dig input_file_path [options]\"\n\texample: \"dig main.dg -o main.exe\"\n\
+#define USAGE_HELPER "Usage: dig input_file_path [options]\n\texamples:\n\
+\t\tdig main.dg -o main.exe\n\
+\t\tdig main.dg\n\
+\t\tdig -h\n\
 Options:\n\
 \t-o <file>\t\t\tSet output file to <file>, the default will be the file name but with .exe extension.\n\
-\t-h, --help\t\t\tShow the help page, including this text.\n\
+\t-h\t\t\t\tShows this message.\n\
+\t-v\t\t\t\tShows the version of the compiler.\n\
 \n\
 \t-E\t\t\tPreprocess only.\n\
 "
+
+ // Max 32 command line arguments
+ #define CMD_OPTIONS_STRING "o:hve" /* all the options */
+class cmd_args
+{
+public:
+    static const int None	= 0;
+    static const int _o		= (1 << 0);
+    static const int _h		= (1 << 1);
+    static const int _v		= (1 << 2);
+    static const int _E		= (1 << 3);
+};
 
 #define ERROR_WE_DONT_KNOW "Something went horribly wrong, probably a bug in the compiler itself, sorry"
 #define UNEXPECTED_CHARACTER(ch) "We reached an unexpected character: '%c'", ch
@@ -25,14 +35,12 @@ Options:\n\
 
 #define IS_HEXA_DIGIT(c) ( (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') )
 
- // Max 32 command line arguments
-class CmdArgs
-{
-public:
-    static const unsigned None	= 0;
-    static const unsigned _o		= (1 << 0);
-    static const unsigned _h		= (1 << 1);
-    static const unsigned _E		= (1 << 2);
-};
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 #endif // MACROS_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -3,54 +3,65 @@
 #include "transpiler/transpiler.hpp"
 #include "preprocessor/preprocessor.hpp"
 
+#include <iostream>
 #include <string>
 #include <fstream>
 #include <streambuf>
 #include <string.h>
+#include <getopt.h>
 
 using std::string;
 
 string getfile(string name);
 inline bool does_file_exist(string path);
+inline void get_options(int argc, char** argv, int &opts, string& path, string& _o);
 
 int main(int argc, char** argv)
 {
-	unsigned cmd_options = static_cast<unsigned>(CmdArgs::None);
-	string path;
+	int cmd_options = cmd_args::None;
+	string path = "";
 	string output_file;
 	string src;
+	bool dont_compile = false;
 
-	// Check path and options
+	//  -----=+*/ PARSE COMMAND LINE ARGUMENTS \*+=-----
+	// Make sure the are command line arguments
 	if (argc < 2) { fprintf(stderr, "No input file\n\n" USAGE_HELPER); exit(-1); }
-	for (size_t i = 2; i < argc; i++)
-	{
-		if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) 	cmd_options |= CmdArgs::_h;
-		if (!strcmp(argv[i], "-E")) 								cmd_options |= CmdArgs::_E;
-		if (!strcmp(argv[i], "-o")) { output_file = argv[i+1];		cmd_options |= CmdArgs::_o; }
-	}
-	
-
-	// Get file path
-	path = argv[1];
-	if (!does_file_exist(path)) { fprintf(stderr, "Input file doesn't exist\n\n" USAGE_HELPER); exit(-1); }
-	// set output_file
-	if (cmd_options & CmdArgs::_o)
-		output_file = "";
+	// Get the path, unless the first argument is a flag, then we'll stop comipilng before getting the file
+	if (argv[1][0] != '-')
+		path.assign(argv[1]);
 	else
-		output_file = path.substr(0, path.find_last_of('.')) + ".exe"; // FIXME: might not be correct
+		dont_compile = true;
+	// Get the options
+	get_options(argc, argv, cmd_options, path, output_file);
 
-	// TODO: Bother with implementing options
+	//  -----=+*/ IMPLEMENT THE COMMAND LINE FLAGS THAT WE CAN \*+=-----
+	// If the -h flag is set, print the help page and continue
+	if (cmd_options & cmd_args::_h) { printf("%s", USAGE_HELPER); }
+	// If the -v flag is set, print the version continue
+	if (cmd_options & cmd_args::_v) { printf("%s", "Dig version: " DIG_VERSION "\n"); }
 
+	// If the don't compile variable is true - exit here with exit code 0
+	if (dont_compile)
+		exit(0);
+
+	//  -----=+*/ GET THE FILE AND SET THE OUTPUT FILE NEEDED \*+=-----
+	// Check if file exist
+	if (!does_file_exist(path)) { fprintf(stderr, "Input file \"%s\" doesn't exist\n\n", path.c_str()); exit(-1); }
+	// set output_file if needed
+	if (!(cmd_options & cmd_args::_o))
+		output_file = path.substr(0, path.find_last_of('.')) + ".exe";
 	// Get the file
 	src = getfile(path);
+
+	//  -----=+*/ BEGINNING OF THE COMPILATION PROCESS! \*+=-----
 
 	// Preprocess
 	preprocess(src);
 
 	// If the -E flag is set, print the code and exit with exit code 0
-	if (cmd_options & CmdArgs::_E) { printf("%s", src.c_str()); exit(0); }
+	if (cmd_options & cmd_args::_E) { printf("%s", src.c_str()); exit(0); }
 
-	// Begging of actual compilation process
 	Lexer lexer(src, path);
 	//Parser parser;
 	//Transpiler transpiler;
@@ -83,4 +94,38 @@ inline bool does_file_exist(string path)
 {
 	std::ifstream f(path.c_str());
 	return f.good();
+}
+
+inline void get_options(int argc, char** argv, int &opts, string& path, string& _o)
+{
+	int opt;
+
+	while ((opt = getopt(argc, argv, ":" CMD_OPTIONS_STRING)) != -1)
+    {
+        switch(opt)
+        {
+            case 'o':
+                opts |= cmd_args::_o;
+				_o.assign(optarg);
+                break;
+            case 'h':
+                opts |= cmd_args::_h;
+                break;
+            case 'v':
+                opts |= cmd_args::_v;
+                break;
+            case 'E':
+                opts |= cmd_args::_E;
+                break;
+            case ':':
+                printf("Option -%c requires a value.\n", optopt);
+                exit(-1);
+            case '?':
+                printf("Unknown option: -%c.\n", optopt);
+                exit(-1);
+			default:
+				fprintf(stderr, ERROR_WE_DONT_KNOW);
+				exit(-1);
+        }
+    }
 }
